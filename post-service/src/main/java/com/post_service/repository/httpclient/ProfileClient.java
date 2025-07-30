884cca020261139c09a30f226348b8d8fd837ec5
quocdong1710 <quocdong989@gmail.com>
2025-07-30 22:52:59 +0700

    fix post service

--- post-service/src/main/java/com/post_service/repository/httpclient/ProfileClient.java
+++ post-service/src/main/java/com/post_service/repository/httpclient/ProfileClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.post_service.dto.response.APIResponse;
 import com.post_service.dto.response.UserProfileResponse;
 
-@FeignClient(name = "profile-service", url = "${app.services.profile-service.url}")
+@FeignClient(name = "profile-service", url = "${app.services.profile.url}")
 public interface ProfileClient {
     @GetMapping("/internal/users/{userId}")
     APIResponse<UserProfileResponse> getProfile(@PathVariable String userId);
